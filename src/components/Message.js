3d11e3d5a935acf65b3119bce8adf2079d7d98dd
GreenPlanet0204 <spearbig1021@gmail.com>
2023-09-15 05:19:35 +0900

    update

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState, useRef } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { collection, getDocs, addDoc, doc, setDoc } from "firebase/firestore";
+import { collection, getDoc, doc, setDoc } from "firebase/firestore";
 import { db } from "../firebase";
+import axios from "axios";
 
 const Message = () => {
   const params = useParams();
@@ -10,6 +11,8 @@ const Message = () => {
   const [text, setText] = useState();
   const [focus, setFocus] = useState(false);
   const [photo, setPhoto] = useState();
+  const [question, setQuestion] = useState();
+  const [location, setLocation] = useState();
 
   const sendResponse = async (data) => {
     await setDoc(
@@ -27,58 +30,51 @@ const Message = () => {
     e.preventDefault();
     const time = (new Date().getTime() / 1000).toFixed(0);
     console.log("time", time);
-    if (params.number) {
+    if (question) {
       const data = {
         response: text,
         epoch: time,
         questionEpoch: params.number,
-        ipAddressLocation: "Los Angeles",
+        ipAddressLocation: location,
       };
       await sendResponse(data);
+      navigate("/p/sent");
     }
-
-    // set(ref(db, "responses/"));
-    navigate("/p/sent");
   };
+
+  const getLocation = async () => {
+    await axios
+      .get("https://geolocation-db.com/json/")
+      .then((res) => {
+        console.log("data", res.data);
+        setLocation(res.data.state);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    getLocation();
+  }, []);
 
   const fetchData = async () => {
     setLoading(true);
     try {
-      await getDocs(collection(db, "questions")).then((querySnapshot) => {
-        const newData = querySnapshot.docs.map((doc) => ({
-          ...doc.data(),
-          id: doc.id,
-        }));
-        if (params.number) {
-          var data = newData.filter(
-            (item) => item.id == `${params.name}-${params.number}`
-          );
-        } else {
-          var data = newData.filter((item) =>
-            item.id.startsWith(params.name + "-")
-          );
-        }
-        if (data.length > 0) {
-          var color = `${data[0].topColor} linear-gradient(to bottom right, ${data[0].topColor} 0%, ${data[0].bottomColor} 100%)`;
-        } else {
-          var color = "transparent";
-        }
-        document.getElementById("root").style.background = color;
-        setLoading(false);
-      });
+      const docSnap = await getDoc(
+        doc(db, "questions", `${params.name}-${params.number}`)
+      );
+      const data = docSnap.data();
+      if (data) {
+        var color = `${data[0].topColor} linear-gradient(to bottom right, ${data[0].topColor} 0%, ${data[0].bottomColor} 100%)`;
+        setQuestion(data.question);
+      } else {
+        var color = "transparent";
+      }
+      document.getElementById("root").style.background = color;
+      setLoading(false);
 
       try {
         const response = await fetch(
-          `https://www.instagram.com/${params.name}/?__a=1&__d=1`,
-          {
-            mode: "cors",
-            credentials: "include",
-            headers: {
-              "Content-Type": "application/json",
-              Accept: "application/json",
-              Origin: "http://localhost:3000",
-            },
-          }
+          `https://www.instagram.com/${params.name}/?__a=1&__d=1`
         ); // fetch page
         const htmlString = await response.text(); // get response text
         console.log("string", htmlString);
@@ -111,7 +107,7 @@ const Message = () => {
                   </div>
                   <div className="user-container">
                     <div className="username">@{params.name}</div>
-                    <div className="prompt">send me anonymous messages!</div>
+                    <div className="prompt">{question}</div>
                   </div>
                 </div>
                 <div className="textarea-container">
